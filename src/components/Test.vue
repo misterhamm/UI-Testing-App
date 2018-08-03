851a171e41c0cc7327e42e5ea8ef50ea5f7d677e
Chris Hamm <chris@bizstream.com>
2018-08-03 09:06:40 -0400

    add test styles

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,11 +1,12 @@
 <template>
 
     <div class="test">
-        <h2>Test</h2>
-        <b-button @click="remove" class="delete-test" :disabled="this.$parent.testList[test.index].actions.length > 0" variant="danger" size="sm">Delete Test</b-button>
-        <label>Test Name</label>
-        <b-form-input class="test-name form-control" v-model="test.name"  placeholder="Test name here" type="text"></b-form-input>
-
+        <div class="testname">
+        <h2 v-if="showName">{{test.name}}</h2> 
+        <b-form-input id="testname" class="test-name form-control" v-model="test.name"  placeholder="Test Name" type="text"></b-form-input>
+        <span class="test-name-edit"></span>
+    </div>
+<b-button @click="remove" class="delete-test" :disabled="this.$parent.testList[test.index].actions.length > 0" variant="danger" size="sm">Delete</b-button>
         <b-button @click="addAction(test.index)" class="add-action" variant="secondary" v-b-popover.hover="'Add an action to the test'">Add Action</b-button>
 
         <test-action 
@@ -19,62 +20,67 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import TestAction from './TestAction.vue'
+import Vue from "vue";
+import TestAction from "./TestAction.vue";
 
 export default {
-    props: ['test'],
-    components:{
-        'test-action': TestAction
+  props: ["test"],
+  components: {
+    "test-action": TestAction
+  },
+  name: "test",
+  data: function() {
+    return {
+      showName: false
+    };
+  },
+  methods: {
+    remove() {
+      this.$emit("remove-test");
     },
-    name: 'test',
-    data: function(){
-        return{
-        }
+    removeAction(testIndex, index) {
+      var counter = 0;
+      var actions = this.$parent.testList[testIndex].actions;
+
+      actions.splice(index, 1);
+      actions.forEach(element => {
+        element.index = counter;
+        counter++;
+      });
     },
-    methods:{
-        remove(){
-            this.$emit('remove-test')
-        },
-        removeAction(testIndex, index){
-            var counter = 0
-            var actions = this.$parent.testList[testIndex].actions
-            
-            actions.splice(index, 1)
-            actions.forEach(element => {
-                element.index = counter
-                counter++
-            });
-            
-        },
-        addAction(testIndex){
-            var actions = this.$parent.testList[testIndex].actions
-            
-            actions.push({index: actions.length, type: '', element: '', name: '', options: ''})
-        }
+    addAction(testIndex) {
+      var actions = this.$parent.testList[testIndex].actions;
+
+      actions.push({
+        index: actions.length,
+        type: "",
+        element: "",
+        name: "",
+        options: ""
+      });
     }
-}
+  }
+};
 </script>
 
 <style lang="scss">
-    .test{
-        
-        
-        .add-action{
-            // float: right;
-        }
+.test {
+  .add-action {
+    // float: right;
+  }
 
-        .delete-test{
-            // margin-left: 5%;
-        }
-        label, h2{
-            // float: left;
-            // clear: both;
-        }
-        .test-name{
-            // float: left;
-            // clear: both;
-            // width: 80%;
-        }
-    }
+  .delete-test {
+    // margin-left: 5%;
+  }
+  label,
+  h2 {
+    // float: left;
+    // clear: both;
+  }
+  .test-name {
+    // float: left;
+    // clear: both;
+    // width: 80%;
+  }
+}
 </style>
